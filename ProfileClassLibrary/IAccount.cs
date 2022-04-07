35a733ea637e0af24e1eaca5ba3ed84b0345d5fc
Tolya <tolya.bob@inbox.ru>
2022-04-07 16:01:56 +0300

    123

---
+++ ProfileClassLibrary/IAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfileClassLibrary
+{
+    internal interface IAccount
+    {
+        string Email { get; }
+        string ReserveEmail { get; }
+        int Phone { get; }
+        void ChangeName(string newName);
+        void ChangeLastName(string newLastName);
+        void ChangePotronymic(string newPatronymic);
+        void ChangePhone(int newPhone);
+        void ChangePassword(string pass,string newPass,string repeatNewPass);
+        void ChangeEmail(string email);
+        void ChangeReserveEmail(string newResEmail);
+    }
+}
---
+++ ProfileClassLibrary/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfileClassLibrary
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+    public abstract class Person
+    {
+        Gender _gender;
+        protected string _name,_lastname,_patronymic;
+        DateTime _birthDate;
+        public Person(Gender gender, string name, string lastname, string patronymic, DateTime birthDate)
+        {
+            _gender = gender;
+            _name = name;
+            _lastname = lastname;
+            _patronymic = patronymic;
+            _birthDate = birthDate;
+        }
+        public int Id { get;set; }
+        public string LastName
+        {
+            get { return _lastname; }
+        }
+        public string Patronymic
+        {
+            get { return _patronymic; }
+        }
+        public string Name
+        {
+            get { return _name;}
+        }
+        public Gender Gender
+        {
+            get { return _gender; }
+        }
+        public DateTime BirthDate
+        {
+            get { return _birthDate;}
+        }
+    }
+}
