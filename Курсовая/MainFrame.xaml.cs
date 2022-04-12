ec5f495ac9b3d46688ec1186d5ecaced7a83cf8e
gorboveckirill@gmail.com <gorboveckirill@gmail.com>
2022-04-12 17:50:29 +0300

    Всё гуд

--- Курсовая/MainFrame.xaml.cs
+++ Курсовая/MainFrame.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Курсовая.MainFrameForms;
 using Курсовая.Setting;
 
 namespace Курсовая
@@ -146,7 +147,7 @@ namespace Курсовая
 
         private void rdNotes_Click(object sender, RoutedEventArgs e)
         {
-            PagesNavigation.Navigate(SityBusPage);
+            PagesNavigation.Navigate(TrainPage);
         }
 
 
